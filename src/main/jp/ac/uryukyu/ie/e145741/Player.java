e4aa15d1fe63f1e2695f392c9f8dfc4eef3307d4
naga55 <e145741@ie.u-ryukyu.ac.jp>
2016-12-20 11:41:16 +0900

    prog2-ex3 マップを表示 マップを配列に１行ずつ格納

---
+++ src/main/jp/ac/uryukyu/ie/e145741/Player.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e145741;
+
+/**
+ * Created by e145741 on 2016/12/20.
+ */
+public class Player extends Maze {
+    int x,y;
+
+    public void F1(){
+        System.out.println(strarray[0]);
+}
